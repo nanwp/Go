24d52cef8e5eabd924d6827b05ded6e4f8c46118
nanda <nandawijaya@apps.ipb.ac.id>
2023-01-13 18:20:44 +0700

    Day 1

---
+++ first-project/main.go
@@ -0,0 +1,9 @@
+package main // package main, executable 
+
+import "fmt" // import package untuk i/o
+
+func main(){ // function main, dijalankan, ketika program dijalankan 
+
+	fmt.Println("Haloo adik") // print
+
+}
